fe39718a9f0c2a8e47fe3cac6ca1b9350477660a
tiliang <mightbxg@hotmail.com>
2021-06-16 14:11:57 +0800

    show ref

--- samples/calibrate_camera.cpp
+++ samples/calibrate_camera.cpp
@@ -23,6 +23,14 @@ public:
 
 } //namespace cv
 
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& vec)
+{
+    for (const auto& v : vec)
+        os << v << " ";
+    return os;
+}
+
 int main(int argc, char* argv[])
 {
     // load data from file
@@ -44,13 +52,13 @@ int main(int argc, char* argv[])
         CV_Assert(vpts3d.size() == vpts2d.size());
     }
 
-    vector<double> params = { 631.6558837890625, 632.6558837890625, 637.0838623046875, 390.05694580078125,
+    vector<double> params_ref = { 631.6558837890625, 632.6558837890625, 637.0838623046875, 390.05694580078125,
         -0.03983112892232504, 0.03768857717889241, 0.0, 0.0007003028120895412, -0.0032084429231644357 };
-    auto success = bxg::CameraCalibrator::optimize(vpts3d, vpts2d, params);
-    cout << "result:\n";
-    for (double v : params)
-        cout << v << " ";
-    cout << endl;
+    vector<double> params = { 600.0, 600.0, 640.0, 400.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+    bxg::CameraCalibrator::optimize(vpts3d, vpts2d, params);
+
+    cout << "refere: " << params_ref << '\n';
+    cout << "result: " << params << '\n';
 
     return 0;
 }
