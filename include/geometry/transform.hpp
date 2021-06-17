ce67b11022d5385417d9f835360ea23fd5ddc787
tiliang <mightbxg@hotmail.com>
2021-06-17 19:31:07 +0800

    complete analytical cost function

--- include/geometry/transform.hpp
+++ include/geometry/transform.hpp
@@ -6,6 +6,8 @@
 
 namespace bxg {
 
+#define USE_PERTURBATION_MODEL 0
+
 template <typename Scalar_ = double>
 class RigidTransform {
 public:
@@ -31,6 +33,13 @@ public:
         return Eigen::AngleAxis<Scalar>(rvec.norm(), rvec.normalized()).toRotationMatrix();
     }
 
+    static inline Vec3 toRotationVector(const Mat33& rmat)
+    {
+        Eigen::AngleAxis<Scalar> aa;
+        aa.fromRotationMatrix(rmat);
+        return aa.axis() * aa.angle();
+    }
+
     static inline Mat33 skewSym(const Vec3& v)
     {
         Mat33 ret;
@@ -46,7 +55,24 @@ public:
         Vec3 ret = rmat_ * pt + tvec_;
         if (J_param) {
             Eigen::Map<Mat3N> jac(J_param);
+#if USE_PERTURBATION_MODEL
             jac.block(0, 0, 3, 3) = -skewSym(ret);
+#else
+            Eigen::AngleAxis<Scalar> aa;
+            aa.fromRotationMatrix(rmat_);
+            Scalar v1, v2; // v1=sinx/x v2=(1-cosx)/x
+            if (abs(aa.angle()) < Eigen::NumTraits<Scalar>::dummy_precision()) {
+                v1 = Scalar(1);
+                v2 = Scalar(0);
+            } else {
+                v1 = sin(aa.angle()) / aa.angle();
+                v2 = (Scalar(1) - cos(aa.angle())) / aa.angle();
+            }
+            Mat33 J_l = v1 * Mat33::Identity()
+                + (Scalar(1) - v1) * aa.axis() * aa.axis().transpose()
+                + v2 * skewSym(aa.axis());
+            jac.block(0, 0, 3, 3).noalias() = -skewSym(rmat_ * pt) * J_l;
+#endif
             jac.block(0, 3, 3, 3) = Mat33::Identity();
         }
         return ret;
@@ -54,12 +80,24 @@ public:
 
     const RigidTransform operator+(const VecN& p) const
     {
+#if USE_PERTURBATION_MODEL
         auto R = toRotationMatrix(p.template head<3>());
         auto t = p.template tail<3>();
         return RigidTransform(R * rmat_, R * tvec_ + t);
+#else
+        return RigidTransform(params() + p);
+#endif
     }
 
-public:
+    VecN params() const
+    {
+        VecN p;
+        p.template head<3>() = toRotationVector(rmat_);
+        p.template tail<3>() = tvec_;
+        return p;
+    }
+
+private:
     Mat33 rmat_;
     Vec3 tvec_;
 };
